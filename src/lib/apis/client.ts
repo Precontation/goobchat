ecb65ba6ec6cdbd0bc6b0698ab1707770b422695
Sander245 <185006982+Sander245@users.noreply.github.com>
2026-09-16 22:56:20 -0700

    YES i got login WORKING FINALLY(ill have todo sign in with google stuff later 😭) also ill make way better later ok bye

--- src/lib/apis/client.ts
+++ src/lib/apis/client.ts
@@ -1,2 +1,21 @@
-// this, is the main file thing yay and yes, i wont do it in js :(
+// this, is the main file thing yay and yes
 import {createClient, type MatrixClient} from "matrix-js-sdk"
+import { saveSession } from "./session"
+
+const HOMESERVER =  "https://matrix-client.matrix.org";
+let client: MatrixClient | undefined;
+
+export const login = async (username: string, password: string): Promise<void> => {
+
+const tempClient = createClient({baseUrl: HOMESERVER})
+const response = await tempClient.loginRequest({type: "m.login.password", identifier: {type: "m.id.user", user: username}, password, initial_device_display_name: "goobchat"});
+const session = {
+		accessToken: response.access_token,
+		deviceId: response.device_id,
+		userId: response.user_id
+	};
+	await saveSession(session);
+    client = createClient({baseUrl: HOMESERVER, accessToken: session.accessToken, userId: session.userId, deviceId: session.deviceId});
+
+}
+export const getClient = () => client;
